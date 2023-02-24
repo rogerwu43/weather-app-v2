1167c7fc46a622864fd8ad0edcb2dd7c1aa77405
redpuffles <regor.wu@gmail.com>
2023-02-24 19:17:40 +1100

    Created skeleton CSS files and bare skeleton Home page

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CurrentWeatherComponent } from './home/components/current-weather/current-weather.component';
+
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        CurrentWeatherComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
     providers: [],
-    bootstrap: [AppComponent],
+    bootstrap: [
+        AppComponent
+    ],
 })
 export class AppModule {}
---
+++ weather-app/src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'home',
+    templateUrl: './home-component.tpl.html'
+})
+export class HomeComponent {
+
+}
